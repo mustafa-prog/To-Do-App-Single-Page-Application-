b0d681a732c264c25632378a52d46e29b0d64a6a
Mustafa Kurukamci <isbuan51@gmail.com>
2020-09-06 22:41:01 +0200

    Updates

---
+++ precache-manifest.73c71efff23aaa6680375f764c2dd103.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1bbafdfe42f46041b3b1eadf6d91c2",
+    "url": "/SPA-todo-app/index.html"
+  },
+  {
+    "revision": "cba3e0ec789715b4d8fc",
+    "url": "/SPA-todo-app/static/css/main.88f9d85b.chunk.css"
+  },
+  {
+    "revision": "cff12cd587da29a233ee",
+    "url": "/SPA-todo-app/static/js/2.f4c9fba0.chunk.js"
+  },
+  {
+    "revision": "1e8c639be0d055b48b146f697441b021",
+    "url": "/SPA-todo-app/static/js/2.f4c9fba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cba3e0ec789715b4d8fc",
+    "url": "/SPA-todo-app/static/js/main.73e2944f.chunk.js"
+  },
+  {
+    "revision": "609caa454de732c6bfd0",
+    "url": "/SPA-todo-app/static/js/runtime-main.db33ed8a.js"
+  }
+]);
